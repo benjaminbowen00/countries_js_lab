51001e370a394c1095b2afa3f8875a09dd87c240
fraserbrown87 <fraserbrown87@gmail.com>
2018-01-31 14:04:44 +0000

    Displaying country name, population and capital

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+var countries
+
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
   request.open('GET', url);
@@ -16,9 +18,10 @@ var populateList = function(countries){
 
 var formDropdown = function(countries){
   var select = document.querySelector('#country-dropdown')
-  countries.forEach(function(country){
+  countries.forEach(function(country, index){
     var option = document.createElement('option');
     option.innerText = country.name;
+    option.value = index;
     select.appendChild(option);
   })
 }
@@ -28,11 +31,11 @@ var requestComplete = function(){
   // console.log(this);
   if (this.status !== 200) {return;}
   var jsonString = this.responseText;
-  var countries = JSON.parse(jsonString);
-  var country = countries[0];
+  countries = JSON.parse(jsonString);
+  // var country = countries[0];
   // console.log(jsonString);
   // console.log(countries);
-  console.log(country);
+  // console.log(country);
   // populateList(countries);
   formDropdown(countries);
 }
@@ -47,12 +50,22 @@ var populateDropdown = function(){
   makeRequest(url, requestComplete);
 }
 
+var handleOptionSelected = function(country){
+  var pTag = document.querySelector('#select-result');
+  var country = countries[this.value];
+  pTag.innerText = `Country: ${country.name} \n Population: ${country.population} \n Capital: ${country.capital}`;
+
+}
+
 
 var app = function(){
   // var showCountriesButton = document.getElementById("button-show-countries");
   // showCountriesButton.addEventListener('click', showCountries);
 
   populateDropdown();
+
+  var select = document.querySelector('#country-dropdown');
+  select.addEventListener('change', handleOptionSelected);
 
   // var url = "https://restcountries.eu/rest/v2";
   // makeRequest(url, requestComplete);
