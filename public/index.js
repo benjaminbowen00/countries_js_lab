6d3f507c679b4e611f98a2e58a040aad3e5b5cc2
benjaminbowen00 <benjibowen@hotmail.com>
2018-01-31 13:35:30 +0000

    dropdown created

--- public/index.js
+++ public/index.js
@@ -14,6 +14,15 @@ var populateList = function(countries){
   });
 }
 
+var formDropdown = function(countries){
+  var select = document.querySelector('#country-dropdown')
+  countries.forEach(function(country){
+    var option = document.createElement('option');
+    option.innerText = country.name;
+    select.appendChild(option);
+  })
+}
+
 var requestComplete = function(){
   // console.log("Check request is working");
   // console.log(this);
@@ -24,7 +33,8 @@ var requestComplete = function(){
   // console.log(jsonString);
   // console.log(countries);
   console.log(country);
-  populateList(countries);
+  // populateList(countries);
+  formDropdown(countries);
 }
 
 var showCountries = function(){
@@ -32,10 +42,17 @@ var showCountries = function(){
   makeRequest(url, requestComplete);
 }
 
+var populateDropdown = function(){
+  var url = "https://restcountries.eu/rest/v2";
+  makeRequest(url, requestComplete);
+}
+
 
 var app = function(){
-  var showCountriesButton = document.getElementById("button-show-countries");
-  showCountriesButton.addEventListener('click', showCountries);
+  // var showCountriesButton = document.getElementById("button-show-countries");
+  // showCountriesButton.addEventListener('click', showCountries);
+
+  populateDropdown();
 
   // var url = "https://restcountries.eu/rest/v2";
   // makeRequest(url, requestComplete);
